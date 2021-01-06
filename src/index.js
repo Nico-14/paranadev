709acd5e9578b31c21500ea1dd80b74c53d76415
BuildTools <mateo.14.ledesma@gmail.com>
2021-01-06 00:13:48 -0300

    responsive terminado + menu

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import 'normalize.css';
 import './styles/style.scss';
 
 const navbar = document.getElementById('navbar');
-window.handleLinkClick = function (linkElement) {
+const menuButton = document.getElementById('menu-button');
+const handleLinkClick = ({ currentTarget }) => {
   const currentActiveLink = navbar.querySelector('a.active');
   if (currentActiveLink) currentActiveLink.classList.remove('active');
-  navbar.classList.remove('show-menu');
-  linkElement.classList.add('active');
+  navbar.classList.remove('is-showing');
+  menuButton.classList.remove('is-showing');
+  currentTarget.classList.add('active');
 };
+
+for (const navbarLink of navbar.querySelectorAll('#navbar .nav__link')) {
+  navbarLink.addEventListener('click', handleLinkClick);
+}
+
+const handleMenuClick = () => {
+  navbar.classList.toggle('is-showing');
+  menuButton.classList.toggle('is-showing');
+};
+menuButton.addEventListener('click', handleMenuClick);
