12a174b503005fec13f2a4c1c87a6493d02ee6f9
BuildTools <mateo.14.ledesma@gmail.com>
2021-01-06 04:23:44 -0300

    add og, up btn, footer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import 'normalize.css';
 import './styles/style.scss';
 
 const navbar = document.getElementById('navbar');
+const goToTopButton = document.getElementById('go-to-top-button');
 const menuButton = document.getElementById('menu-button');
-const handleLinkClick = ({ currentTarget }) => {
+
+const removeCurrentActiveLink = () => {
   const currentActiveLink = navbar.querySelector('a.active');
   if (currentActiveLink) currentActiveLink.classList.remove('active');
+};
+
+const handleLinkClick = ({ currentTarget }) => {
+  removeCurrentActiveLink();
   navbar.classList.remove('is-showing');
   menuButton.classList.remove('is-showing');
   currentTarget.classList.add('active');
@@ -20,3 +26,21 @@ const handleMenuClick = () => {
   menuButton.classList.toggle('is-showing');
 };
 menuButton.addEventListener('click', handleMenuClick);
+
+const handleGoToTopClick = () => {
+  removeCurrentActiveLink();
+  navbar.children[0].classList.add('active');
+  window.scrollTo(0, 0);
+  location.hash = 'inicio';
+};
+goToTopButton.addEventListener('click', handleGoToTopClick);
+
+addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    goToTopButton.style.opacity = '1';
+    goToTopButton.style.visibility = 'visible';
+  } else {
+    goToTopButton.style.opacity = '0';
+    goToTopButton.style.visibility = 'hidden';
+  }
+});
