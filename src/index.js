969c394895df36aaf7a9cce3ed193dcfa754a5cf
BuildTools <mateo.14.ledesma@gmail.com>
2021-01-06 21:14:51 -0300

    ilustraciones + javascript

--- src/index.js
+++ src/index.js
@@ -10,11 +10,15 @@ const removeCurrentActiveLink = () => {
   if (currentActiveLink) currentActiveLink.classList.remove('active');
 };
 
-const handleLinkClick = ({ currentTarget }) => {
+const handleLinkClick = (e) => {
+  e.preventDefault();
   removeCurrentActiveLink();
   navbar.classList.remove('is-showing');
   menuButton.classList.remove('is-showing');
-  currentTarget.classList.add('active');
+  e.currentTarget.classList.add('active');
+
+  const section = document.getElementById(e.currentTarget.dataset.target);
+  section?.scrollIntoView();
 };
 
 for (const navbarLink of navbar.querySelectorAll('#navbar .nav__link')) {
@@ -28,19 +32,35 @@ const handleMenuClick = () => {
 menuButton.addEventListener('click', handleMenuClick);
 
 const handleGoToTopClick = () => {
-  removeCurrentActiveLink();
-  navbar.children[0].classList.add('active');
   window.scrollTo(0, 0);
-  location.hash = 'inicio';
 };
 goToTopButton.addEventListener('click', handleGoToTopClick);
 
-addEventListener('scroll', () => {
-  if (window.scrollY > 200) {
-    goToTopButton.style.opacity = '1';
-    goToTopButton.style.visibility = 'visible';
-  } else {
-    goToTopButton.style.opacity = '0';
-    goToTopButton.style.visibility = 'hidden';
-  }
-});
+let observer = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        /*        if ('#' + entry.target.id != location.hash) {
+          history.pushState(null, null, '#' + entry.target.id);
+          removeCurrentActiveLink();
+          navbar.querySelector(`a[href="${location.hash}"]`)?.classList.add('active');
+        } */
+        removeCurrentActiveLink();
+        navbar.querySelector(`a[data-target=${entry.target.id}]`)?.classList.add('active');
+        if (entry.target.id != 'inicio') {
+          goToTopButton.style.opacity = '1';
+          goToTopButton.style.visibility = 'visible';
+        } else {
+          goToTopButton.style.opacity = '0';
+          goToTopButton.style.visibility = 'hidden';
+        }
+      }
+    }
+  },
+
+  { threshold: 0.5 }
+);
+
+observer.observe(document.getElementById('inicio'));
+observer.observe(document.getElementById('nosotros'));
+observer.observe(document.getElementById('servicios'));
